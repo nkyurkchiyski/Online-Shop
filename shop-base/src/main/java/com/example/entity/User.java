186a6760fc19a7532663aa48c791aa599f063de7
nkyurkchiyski <n.kyurkchiyski@gmail.com>
2019-10-22 23:27:17 +0300

    Added annotations for User

--- shop-base/src/main/java/com/example/entity/User.java
+++ shop-base/src/main/java/com/example/entity/User.java
@@ -8,13 +8,124 @@
 package com.example.entity;
 
 
-import java.io.Serializable;
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Set;
 
-import javax.persistence.Table;
+@Entity
+@Table(name = "t_User")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cUserId")
+    private int id;
 
-@Table(name = "")
-public class User implements Serializable
-{
-    private static final long serialVersionUID = 1L;
+    @Column(name = "cUserName")
+    private String username;
 
+    @Column(name = "cUserFirstName")
+    private String firstName;
+
+    @Column(name = "cUserLastName")
+    private String lastName;
+
+    @Column(name = "cUserEmail")
+    private String email;
+
+    @Column(name = "cUserPassword")
+    private String password;
+
+    @Column(name = "cUserCreatedAt")
+    private LocalDate createdAt;
+
+    @Column(name = "cUserActive")
+    private boolean isActive;
+
+    private Set<Role> roles;
+
+    @OneToMany(mappedBy = "user", targetEntity = Order.class)
+    private Set<Order> orders;
+
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDate getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDate createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    public Set<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(Set<Order> orders) {
+        this.orders = orders;
+    }
 }
