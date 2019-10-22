d0cca24ed00e7770fff0d28da27d6e44d3d07c8a
Nikolay Kyurkchiyski <n.kyurkchiyski@seeburger.de>
2019-10-22 12:43:39 +0300

    Initial Commit

---
+++ shop-base/src/main/java/com/example/entity/User.java
@@ -0,0 +1,20 @@
+/*
+ * User.java
+ *
+ * created at 2019-10-22 by n.kyurkchiyski <YOURMAILADDRESS>
+ *
+ * Copyright (c) SEEBURGER AG, Germany. All Rights Reserved.
+ */
+package com.example.entity;
+
+
+import java.io.Serializable;
+
+import javax.persistence.Table;
+
+@Table(name = "")
+public class User implements Serializable
+{
+    private static final long serialVersionUID = 1L;
+
+}
